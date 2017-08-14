21e1be87b3d49231daa8d6ad590338e144cf33f4
one-for-all <jianan.li@yahoo.com>
2017-08-14 13:27:08 -0700

    remove venv

--- project8_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jianan/Developer/WebDev/teamtreehouse/python_track/project8-minerals_search/project8_env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
